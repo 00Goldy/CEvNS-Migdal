Fixing normalization of Migdal rate

week3/functions_cevns.py
@@ -9,7 +9,6 @@ from scipy.interpolate import interp1d
 
 N = 77
 Z = 54
-G_F = 1.1664e-11 # MeV^-2 Fermi's constant
 M_Xe = 131.293 
 m_A = M_Xe * 931.5  # MeV
 Q_V = N - 0.1084 * Z
@@ -27,13 +26,16 @@ def dsigma_dER(E_v, E_R) :
     ER_max = (2 * E_v**2) / (m_A + 2 * E_v)
     if E_R > ER_max:
         dsigma.append(0) #There is no CEvNS past ER_max so no cross-section
-
+        print("CPT")
     else:
-
         val = (G_F**2 * m_A) / (4 * np.pi) * Q_V**2 * (1 - ((m_A * E_R) / (2 * E_v**2)))
         dsigma.append(val)
     dsigma = np.array(dsigma) #convert into an np.array for easy plotting
-
+    #print(dsigma)
+    #print('Pipou')
+    #print(E_v)
+    #print('Pipoudou')
+    #print(E_R)
     return dsigma
 
 ################### Now for the total solar neutrino flux
@@ -145,7 +147,6 @@ for input_file in os.listdir(path):
 
 def total_flux(Ev):
     Tot = sum(interp(Ev) for interp in interpol_flux.values())
-    Tot *= 3.154e7
     return Tot
 
 

week3/test_rate_migdal.py
@@ -6,24 +6,38 @@ import wimprates as wr
 from functions_cevns import dsigma_dER
 from functions_cevns import total_flux
 
-E_e = np.linspace(0.1 ,1e6,10) * nu.keV
-E_v = np.linspace(0.1,30, 1000) * nu.MeV
-
+E_e = np.linspace(1e-10 ,1e10, 100) #MeV -> eV
+E_v = np.linspace(0.6 ,30, 1000) #MeV
+dsigma = []
 #print(total_flux(0.4))
-#print(dsigma_dER(E_v, 0.005))
+for Ev in E_v :
+    dsigma.append(dsigma_dER(Ev, 0.005))
+
+
+plt.plot(E_v, dsigma, ":",label='Cross section', color='black')
+plt.title(r"CS VS $E_\nu$")
+plt.xlabel(r"Neutrino Energy $E_\nu$")
+plt.ylabel("Cross section")
+#plt.yscale('log')
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.show()
 
 
 Rate = wr.rate_migdal_cevns(
     E_e,
     total_flux,
     dsigma_dER,
+    E_nu_min = 1e-9,
 )
 
 plt.plot(E_e, Rate, ":",label='Migdal Rate', color='black')
 plt.title(r"Migdal Rate VS $E_e$")
 plt.xlabel(r"Electron Energy $E_e$")
 plt.ylabel("Migdal Rate")
-#plt.yscale('log')
+plt.xscale('log')
+plt.yscale('log')
 plt.legend()
 plt.grid(True)
 plt.tight_layout()